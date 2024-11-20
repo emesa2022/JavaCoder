1c885b15fbfce4e8c9a6b5869c46aa9af428091a
exequiel <exequielgcba@gmail.com>
2024-11-19 23:20:47 -0300

    ejercicios

--- clase-01/src/main/java/com/coderhouse/Clase01.java
+++ clase-01/src/main/java/com/coderhouse/Clase01.java
@@ -3,8 +3,34 @@ package com.coderhouse;
 public class Clase01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hola de vuelta2");
-
+		
+		//respuesta variables
+		int edad;
+		final int DIA_SEMANA;
+		double precioProducto;
+		boolean estudianteAprobado;
+		char inicialNombre;
+		double[] notas = new double[5];
+		String nombreCompleto;
+		float peso;
+		
+		
+		
+		//respuesta ejercicio constante
+		final double PI = 3.14;
+		final int CANTIDADALUMNOS ;
+		
+		//respuesta operadores
+		int numero1 = 5;
+		int numero2 = 5;
+		System.out.println(numero1 + numero2);
+		
+		
+		double numero3 = 6;
+		double numero4 = 5;
+		System.out.println(numero3 - numero4);
+		
+		
 	}
 
 }
